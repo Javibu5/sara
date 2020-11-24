71421b4e6cdd383bea1d7f4770185ee77259fa5c
Javi <adfjb77@gmail.com>
2020-11-24 19:38:03 +0100

    Add Admin package

---
+++ packages/admin/src/App.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Admin } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const App = () => <Admin dataProvider={dataProvider} />;
+
+export default App;
